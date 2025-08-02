4f1004f01eb055a73b91073392026f2be3cdb8b7
sanchuuk <164579869+sanchuuk@users.noreply.github.com>
2025-08-02 17:17:24 +0100

    Updated vite.config.ts

--- project 2/vite.config.ts
+++ project 2/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['lucide-react'], // ✅ On force Vite à optimiser lucide-react
+  },
 });
